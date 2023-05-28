023e15393f91aeebe6072a951f97585185813169
JJPerry <perryjj2803@gmail.com>
2023-05-28 21:09:22 +0800

    Assignment.compelte
    forgot queue

---
+++ Assignment/DSAQueue.java
@@ -0,0 +1,106 @@
+import java.util.*;
+import java.util.Arrays;
+public class DSAQueue
+{
+    private Object[] queue;
+    private Object value;
+    private int front, rear;
+    public boolean empty, full;
+    private double topVal;
+    private int count;
+
+    public static final int DEFAULT_CAPACITY = 100;
+ 
+    public DSAQueue()
+    {
+        queue = new Object[DEFAULT_CAPACITY];
+        count = 0;
+    }
+
+    public DSAQueue(int maxCapacity)
+    {
+        queue = new Object[maxCapacity];
+        count = 0;
+    }
+
+
+    public int getCount()
+    {
+        return count;
+    }
+
+    public boolean isEmpty()
+    {
+        if(count != 0)
+        {
+            empty = false;
+        }
+        else
+        {
+            empty = true;
+        }
+        return empty;
+    }
+
+    public boolean isFull()
+    {
+        if(count == queue.length)
+        {
+            full = true;
+        }
+        else
+        {
+            full = false;
+        }
+        return full;
+    }
+
+    public void enqueue(Object value)
+    {
+        if(isFull())
+        {
+            System.out.print( "Queue is full" );
+        }
+        else
+        {
+            queue[rear] = value;
+            rear = rear+1;
+            count = count+1;
+        }
+    }
+
+    public Object dequeue()
+    {
+        if(isEmpty())
+        {
+            throw new IllegalArgumentException( "Queue is empty" );
+        }
+        else
+        {
+            Object value = queue[front];
+            queue[front] = 0;
+            front = front+1;
+            count = count-1;
+    
+            return value;
+        }
+    }
+
+    public double peek()
+    {
+        if(isEmpty())
+        {
+            throw new IllegalArgumentException( "Queue is empty" );
+        }
+        else
+        {
+            Object topVal = queue[count-1];
+        }
+        return topVal;
+    }
+    
+    public void show()
+    {
+        System.out.println(Arrays.toString(queue));
+    }
+}
